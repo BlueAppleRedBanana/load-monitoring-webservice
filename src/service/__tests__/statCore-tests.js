aaa016850b697eb4b66d90c32caf44944a7e7bbc
Jinwoong Hwang <redbanana.blueapple@gmail.com>
2018-11-08 00:48:41 -0500

    test(stat core): fixed test

--- src/service/__tests__/statCore-tests.js
+++ src/service/__tests__/statCore-tests.js
@@ -5,41 +5,39 @@ describe('getAlertStatus', () => {
         parseTickCpuLoad: () => 1.1
     }));
 
-    const cpuStatLoadIsGreaterThanOne = {
-        loadavg: { '1m': 1.1 },
-        cpus: [1],
-    };
+    const sumOfDeltasOverThanZero = [-0.1, 0.1, 0.1];
 
-    const cpuStatLoadIsLessThanOne = {
-        loadavg: { '1m': 0.9 },
-        cpus: [1],
-    };
+    const sumOfDeltasLessThanZero = [-0.1, 0.1, -0.1];
 
     const thresholdAsOne = 1;
 
-    it('returns alert true if cpuLoad is greater than threshold', () => {
-        const { alert } = getAlertStatus(cpuStatLoadIsGreaterThanOne, true, thresholdAsOne);
+    it('do not trigger alert if recent record is less than 12', () => {
+
+    });
+
+    it('returns alert true if deltas sum is greater than zero', () => {
+        const { alert } = getAlertStatus(sumOfDeltasOverThanZero, true);
         expect(alert).toEqual(true);
     });
 
-    it('returns alert false if cpuLoad is less than threshold', () => {
-        const { alert } = getAlertStatus(cpuStatLoadIsLessThanOne, true, thresholdAsOne);
+    it('returns alert false if deltas sum is less than zero', () => {
+        const { alert } = getAlertStatus(sumOfDeltasLessThanZero, true);
         expect(alert).toEqual(false);
     });
 
     it('returns isAlertUpdated true if currentAlert is different to new alert', () => {
-        let result = getAlertStatus(cpuStatLoadIsGreaterThanOne, false, thresholdAsOne);
+        let result = getAlertStatus(sumOfDeltasOverThanZero, false, thresholdAsOne);
         expect(result.isAlertUpdated).toEqual(true);
 
-        result = getAlertStatus(cpuStatLoadIsLessThanOne, true, thresholdAsOne);
+        result = getAlertStatus(sumOfDeltasLessThanZero, true, thresholdAsOne);
         expect(result.isAlertUpdated).toEqual(true);
     });
 
     it('returns isAlertUpdated false if currentAlert is same to new alert', () => {
-        let result = getAlertStatus(cpuStatLoadIsGreaterThanOne, true, thresholdAsOne);
+        let result = getAlertStatus(sumOfDeltasOverThanZero, true, thresholdAsOne);
         expect(result.isAlertUpdated).toEqual(false);
 
-        result = getAlertStatus(cpuStatLoadIsLessThanOne, false, thresholdAsOne);
+        result = getAlertStatus(sumOfDeltasLessThanZero, false, thresholdAsOne);
         expect(result.isAlertUpdated).toEqual(false);
     });
 });
